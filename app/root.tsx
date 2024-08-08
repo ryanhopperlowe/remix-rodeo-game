c8ce1a2b19406efb84057bfb835da25e80dfc64d
Ryan Hopper-Lowe <ryan.hopperlowe@smartgistics.com>
2024-08-07 20:32:49 -0500

    commit

--- app/root.tsx
+++ app/root.tsx
@@ -6,22 +6,25 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <ChakraProvider>
+            {children}
+          </ChakraProvider>
+          <ScrollRestoration />
+          <Scripts />
+        </body>
+      </html>
   );
 }
 
