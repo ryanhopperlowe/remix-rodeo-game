f128a44264eba05bc960340db133567da9650565
Erwins Saget <erwinssaget@gmail.com>
2024-08-07 20:15:25 -0500

    add attempts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,42 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from '@remix-run/node';
+import { Form, useLoaderData } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+    return [{ title: 'Re-Wordle' }, { name: 'description', content: 'Welcome to Remix!' }];
+};
+
+export async function loader() {
+    return {
+        attempts: [[], [], [], [], [], []]
+    };
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+    const formData = Object.fromEntries(await request.formData());
+
+    console.log({ formData });
+    return {
+        attempts: [[], [], [], [], [], []]
+    };
+}
+
+const colorMap = {
+    0: 'black',
+    1: 'green',
+    2: 'yellow'
 };
 
 export default function Index() {
-  return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    const { word } = useLoaderData<typeof loader>();
+    return (
+        <div className="font-sans p-4">
+            <Form>
+                <ul className="list-disc mt-4 pl-6 space-y-2">
+                    {word.map((letter, idx) => (
+                        <input key={`letter-${idx}`} name={idx.toString()} />
+                    ))}
+                </ul>
+            </Form>
+        </div>
+    );
 }
