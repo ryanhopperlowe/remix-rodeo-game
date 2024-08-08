38a7af432b4011f8db7262d3152a5747768512b4
Jacob Wolf <jacob@brayll.com>
2024-08-07 20:30:45 -0500

    Removed CUID

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,18 +1,16 @@
-import { pgTable, boolean, timestamp, pgEnum, serial, varchar } from 'drizzle-orm/pg-core';
-import { cuid2 } from 'drizzle-cuid2/postgres';
+import { pgTable, boolean, timestamp, pgEnum, serial, varchar, uuid } from 'drizzle-orm/pg-core';
 
-// Define the enum
 export const gameStatusEnum = pgEnum('game_status', ['in_progress', 'won', 'lost']);
 
 export const words = pgTable('users', {
-    id: cuid2('id').defaultRandom().primaryKey(),
+    id: uuid('id').defaultRandom().primaryKey(),
     word: varchar('word', { length: 5 }).notNull(),
     is_solution: boolean('is_solution').notNull().default(false)
 });
 
 export const games = pgTable('games', {
-    id: cuid2('id').defaultRandom().primaryKey(),
-    solution_word_id: cuid2('solution_word_id')
+    id: uuid('id').defaultRandom().primaryKey(),
+    solution_word_id: uuid('solution_word_id')
         .notNull()
         .references(() => words.id),
     started_at: timestamp('started_at').notNull().defaultNow(),
@@ -21,8 +19,8 @@ export const games = pgTable('games', {
 });
 
 export const guesses = pgTable('guesses', {
-    id: cuid2('id').defaultRandom().primaryKey(),
-    game_id: cuid2('game_id')
+    id: uuid('id').defaultRandom().primaryKey(),
+    game_id: uuid('game_id')
         .notNull()
         .references(() => games.id),
     word: varchar('word', { length: 5 }).notNull(),
